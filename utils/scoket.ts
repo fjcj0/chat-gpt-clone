6c5de35da4d9d84da7583168336bc5ca95dabaea
omar <omarqais225@gmail.com>
2025-11-09 16:06:21 +0200

    Fix socket not conncted

--- utils/scoket.ts
+++ utils/scoket.ts
@@ -1,4 +1,17 @@
 import { io } from "socket.io-client";
-export const socket = io(process.env.EXPO_PUBLIC_SERVER_URL, {
+const serverUrl = process.env.EXPO_PUBLIC_SERVER_URL;
+export const socket = io(serverUrl, {
     withCredentials: true,
+});
+socket.on('connect', () => {
+    console.log('Socket connected to:', serverUrl);
+});
+socket.on('disconnect', (reason) => {
+    console.log('Socket disconnected:', reason);
+});
+socket.on('connect_error', (error) => {
+    console.error('Socket connection error:', error.message);
+});
+socket.on('error', (error) => {
+    console.error('Socket error:', error);
 });
